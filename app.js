fd3141a43c53353738ef5e9502db98dbdcf8863d
Chris Sompa <ctsompa@gmail.com>
2025-08-15 17:08:29 +0100

    change pixel color and user prompts added

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
-const parentNode = document.querySelector('#container');
+// Root node
+const root = document.querySelector('body')
 
+// parent Node
+const parentNode = document.querySelector('#container');
 parentNode.style.display = 'flex';
 parentNode.style.flexWrap = 'wrap';
 parentNode.style.width = '460px';
 
+// Enter number of pixels
+const btnPixel = document.createElement('button');
+const header = document.querySelector('h1');
+btnPixel.classList.add('btn_pixels');
+btnPixel.textContent = 'Enter Pixels';
+root.appendChild(btnPixel);
+header.after(btnPixel);
+
+// Create the 'change color' button
+const btnColor = document.createElement('button');
+btnColor.classList.add('btn_color');
+btnColor.textContent = 'Change Color';
+root.appendChild(btnColor);
+
+// Prompt the number of pixels
+btnPixel.addEventListener('click', function(){
+    const pixels = prompt('Enter the number of pixels:');
+    canvas(pixels);
+});
+
+
+// Let's play!
 function canvas(num){
+    // Add Pixel
     for(let i = 0; i < num; i++){
         const square = document.createElement('div');
-        square.classList.add('square');
+        square.classList.add('pixel');
         square.style.width = '20px';
         square.style.height = '20px';
         square.style.border = '1px solid black';
         square.style.margin = '3px';
         parentNode.appendChild(square);
     }
-}
-
-canvas(256);
-
-// This returns HTML Collections, you'll need to loop over them
-const allSquares = document.getElementsByClassName('square');
-
-for (let i = 0; i < allSquares.length; i++) {
-  allSquares[i].addEventListener('mouseenter', function() {
-    allSquares[i].style.backgroundColor = 'red';
-  });
+    // Start sketching
+    const allPixels = document.getElementsByClassName('pixel');
+    for (let i = 0; i < allPixels.length; i++) {
+        allPixels[i].addEventListener('mouseenter', function() {
+        allPixels[i].style.backgroundColor = 'black';
+        });
+    }
+    // Change color
+    btnColor.addEventListener('click', function(){
+        const color = prompt('What color do you want?');
+        for(let i = 0; i < allPixels.length; i++){
+            allPixels[i].addEventListener('mouseenter', function(){
+            allPixels[i].style.backgroundColor = color;
+        })
+        }
+    });
 }
