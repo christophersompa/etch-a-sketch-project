02a01ac5c8e944b95bee2b5c82cab2fbd123e4f5
Chris Sompa <ctsompa@gmail.com>
2025-08-21 23:07:45 +0100

    style changes

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const root = document.querySelector('body')
-const parentNode = document.querySelector('#container');
+const container = document.querySelector('#container');
+const header = document.querySelector('h1');
 
 const addPxl = document.createElement('button');
-const header = document.querySelector('h1');
 const chgColor = document.createElement('button');
+const canvas = document.createElement('div');
+const operators = document.createElement('div');
 
-parentNode.style.display = 'flex';
-parentNode.style.flexWrap = 'wrap';
-parentNode.style.width = '460px';
+container.appendChild(header);
+
+operators.classList.add('operators');
+container.appendChild(operators);
 
 addPxl.classList.add('addPxl');
 addPxl.textContent = 'Enter Pixels';
-root.appendChild(addPxl);
-header.after(addPxl);
+operators.appendChild(addPxl);
 
-chgColor.classList.add('chgcColor');
+chgColor.classList.add('chgColor');
 chgColor.textContent = 'Change Color';
-root.appendChild(chgColor);
+operators.appendChild(chgColor);
 
-function canvas(num){
+canvas.classList.add('canvas');
+container.appendChild(canvas);
+
+function draw(num){
     for(let i = 0; i < num*num; i++){
         const pxl = document.createElement('div');
         pxl.classList.add('pxl');
-        pxl.style.width = '20px';
-        pxl.style.height = '20px';
+        pxl.style.width = '12px';
+        pxl.style.height = '12px';
         pxl.style.border = '1px solid black';
-        pxl.style.margin = '3px';
-        parentNode.appendChild(pxl);
+        pxl.style.margin = '0.05em';
+        canvas.appendChild(pxl);
     }
 
     const allPxls = document.getElementsByClassName('pxl');
@@ -36,7 +41,7 @@ function canvas(num){
         });
     }
 
-    btnColor.addEventListener('click', function(){
+    chgColor.addEventListener('click', function(){
         const color = prompt('What color do you want?');
         for(let i = 0; i < allPxls.length; i++){
             allPxls[i].addEventListener('mouseenter', function(){
@@ -61,8 +66,8 @@ addPxl.addEventListener('click', function(){
         }
         if(document.querySelectorAll('.pxl').length > 0){
             clear('.pxl');
-            canvas(num);
+            draw(num);
         } else {
-            canvas(num);
+            draw(num);
         }
 });
